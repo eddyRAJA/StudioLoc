6110b72ca6b38da0db20ae182fbc9ccff0b2ecbe
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2022-03-15 12:57:18 +0100

    install ckeditor and use it

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,11 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-//use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
@@ -15,8 +14,7 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('object', TextType::class)
-            ->add('message', TextareaType::class);
-            //->add('message', CKEditorType::class);
+            ->add('message', CKEditorType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
