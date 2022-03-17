36df76c86e01204429f26e97acd17c5096f00b1d
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2022-03-17 16:51:54 +0100

    payment install

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class PaymentController extends AbstractController
+{
+    /**
+     * @Route("/payment", name="payment")
+     */
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/checkout", name="checkout")
+     */
+    public function checkout($stripeSK): Response
+    {
+        Stripe::setApiKey($stripeSK);
+
+        $session = Session::create([
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'Reservations',
+                ],
+                'unit_amount' => 30000,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+          return $this->redirect($session->url,303);
+        
+    }
+
+
+    /**
+     * @Route("/success_url", name="success_url")
+     */
+    public function successUrl(): Response
+    {
+        return $this->render('payment/success.html.twig', []);
+    }
+
+
+    /**
+     * @Route("/cancel_url", name="cancel_url")
+     */
+    public function cancelUrl(): Response
+    {
+        return $this->render('payment/cancel.html.twig', []);
+    }
+}
